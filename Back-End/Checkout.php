06dcf7101fae0aa6b1ed0e8d221c459c291974f1
Krutik19 <krutikpatel822@gmail.com>
2024-07-25 22:00:10 -0400

    Adding order summary on checkout page

--- Back-End/Checkout.php
+++ Back-End/Checkout.php
@@ -12,20 +12,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $billingaddress = $conn->real_escape_string($_POST['billing-address']);
     $billingcity = $conn->real_escape_string($_POST['billing-city']);
     $billingzip = $conn->real_escape_string($_POST['billing-zip']);
+    $totalAmount = $conn->real_escape_string($_POST['totalAmount']);
 
-    $sql = "INSERT INTO CheckOut (FirstName, LastName, billingNumber, Email, billingAddress, city, zip,  created_at) 
-            VALUES ('$billingFirstName', '$billingLastName' ,' $billingNumber' , '$billingEmail' ,'$billingaddress', '$billingcity' ,'$billingzip' , NOW())";
+    $sql = "INSERT INTO CheckOut (FirstName, LastName, billingNumber, Email, billingAddress, city, zip, totalAmount, created_at) 
+            VALUES ('$billingFirstName', '$billingLastName', '$billingNumber', '$billingEmail', '$billingAddress', '$billingCity', '$billingZip', '$totalAmount', NOW())";
 
     if ($conn->query($sql) === TRUE) {
         $_SESSION['message'] = "CheckOut successfully!";
+        
+        header("Location: ../Views/Payment.php?totalAmount=" . urlencode($totalAmount));
+        exit();
     } else {
         $_SESSION['message'] = "Error: " . $sql . "<br>" . $conn->error;
+        
     }
 
     $conn->close();
-
-    header("Location: ../Views/Checkout.php");
-    exit();
 }
 ?>
  
