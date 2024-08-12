244c98ae0b43983d52f045f8bacfcf1a5d9324d0
Krutik19 <krutikpatel822@gmail.com>
2024-08-11 20:22:28 -0400

    updating reservation backend

--- Admin/Back-End/ReservationOperations.php
+++ Admin/Back-End/ReservationOperations.php
@@ -5,7 +5,6 @@ $message = "";
 
 function handlePostRequest($conn) {
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-       
         if (isset($_POST['update'])) {
             $reservation_id = $_POST['reservation_id'];
             $customer_name = $_POST['customer_name'];
@@ -14,18 +13,29 @@ function handlePostRequest($conn) {
             $number_of_guests = $_POST['number_of_guests'];
             $special_requests = $_POST['special_requests'];
 
-            $sql = "UPDATE Reservations SET ";
-            $sql .= "customer_name='" . $customer_name . "', ";
-            $sql .= "email='" . $email . "', ";
-            $sql .= "reservation_date='" . $reservation_date . "', ";
-            $sql .= "number_of_guests=" . $number_of_guests . ", ";
-            $sql .= "special_requests='" . $special_requests . "' ";
-            $sql .= "WHERE reservation_id=" . $reservation_id;
-            
+            $sql = "UPDATE Reservations SET 
+                        customer_name='$customer_name', 
+                        email='$email', 
+                        reservation_date='$reservation_date', 
+                        number_of_guests=$number_of_guests, 
+                        special_requests='$special_requests' 
+                    WHERE reservation_id=$reservation_id";
+
             if ($conn->query($sql) === TRUE) {
                 $message = "Reservation updated successfully";
             } else {
                 $message =  "Error updating reservation: " . $conn->error;
+            }
+        }
+
+        if (isset($_POST['delete'])) {
+            $reservation_id = $_POST['reservation_id'];
+
+            $sql = "DELETE FROM Reservations WHERE reservation_id=$reservation_id";
+            if ($conn->query($sql) === TRUE) {
+                $message = "Reservation deleted successfully";
+            } else {
+                $message = "Error deleting reservation: " . $conn->error;
             }
         }
 
@@ -35,7 +45,6 @@ function handlePostRequest($conn) {
 }
 
 handlePostRequest($conn);
-
 $sql = "SELECT * FROM Reservations";
 $result = $conn->query($sql);
 ?>
